8432aa1de3cf0db84dbaa962632dd9d393c940ed
Juan Riquelme <pk.juanr@gmail.com>
2022-04-17 01:59:28 -0500

    lol

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const http = require("http");
 const cors = require("cors");
 
 const app = express();
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
 app.use(
   cors({
     origin: [
@@ -28,6 +38,7 @@ const io = new Server(server, {
       "https://sockets-jrigoo.netlify.app/",
       "https://jrigoo.github.io/Sockets/",
     ],
+    methods: ["GET", "POST"],
   },
 });
 
