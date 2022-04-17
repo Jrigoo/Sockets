22331f0709095153ee6055a5dc2d1bdb356bfaf5
Juan Riquelme <pk.juanr@gmail.com>
2022-04-17 01:19:38 -0500

    :tada: First Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const { Server } = require("socket.io");
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+
+const app = express();
+app.use(
+  cors({
+    origin: ["*", "http://localhost:5500", "http://127.0.0.1:5500"],
+  })
+);
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: ["*", "http://localhost:5500", "http://127.0.0.1:5500"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("data", (arg, callback) => {
+    io.sockets.emit("client", arg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on 3000");
+});
