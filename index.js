a8afadc307e2c06837a446718fe148bf4ef4f3a1
Juan Riquelme <pk.juanr@gmail.com>
2022-04-17 01:46:13 -0500

    :zap: Lo que sea x2

--- index.js
+++ index.js
@@ -22,7 +22,12 @@ app.get("/", (req, res) => {
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["*", "http://localhost:5500", "http://127.0.0.1:5500"],
+    origin: [
+      "*",
+      "http://localhost:5500",
+      "http://127.0.0.1:5500",
+      "https://regal-phoenix-98fa39.netlify.app/",
+    ],
   },
 });
 
